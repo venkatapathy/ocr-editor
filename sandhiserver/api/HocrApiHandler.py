0d305036956041fc4e1fff8fc53fa6749a9e3fa1
venkatapathy <venkat.s.iyer@gmail.com>
2021-07-03 04:19:11 +0530

    a working server that serves image and hocr based on path

--- sandhiserver/api/HocrApiHandler.py
+++ sandhiserver/api/HocrApiHandler.py
@@ -3,21 +3,21 @@ from flask import send_file
 import os.path
 
 
-class ImageApiHandler(Resource):
+class HocrApiHandler(Resource):
     def get(self, bid, pid):
         image_dir = "./static"
 
         bookname = "b" + str(bid)
-        pagename = "page-" + format(pid, "03d") + ".jpeg"
+        pagename = "page-" + format(pid, "03d") + ".hocr"
         if os.path.isfile(os.path.join(image_dir, bookname, pagename)):
             return send_file(os.path.join(image_dir, bookname, pagename),
-                             mimetype='image/jpeg')
+                             mimetype='text/html')
         else:
             return {
                 "status":
                 "error",
                 "message":
-                "no images found at path " +
+                "no hocr found at path " +
                 os.path.join(image_dir, bookname, pagename)
             }
 
--- sandhiserver/app.py
+++ sandhiserver/app.py
@@ -2,6 +2,7 @@ from flask import Flask, send_from_directory
 from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS  #comment this on deployment
 from api.ImageApiHandler import ImageApiHandler
+from api.HocrApiHandler import HocrApiHandler
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 CORS(app)  #comment this on deployment
@@ -13,4 +14,5 @@ def serve(path):
     return send_from_directory(app.static_folder, 'index.html')
 
 
-api.add_resource(ImageApiHandler, '/b/<bid>/p/<pid>')
+api.add_resource(ImageApiHandler, '/i/b/<int:bid>/p/<int:pid>')
+api.add_resource(HocrApiHandler, '/h/b/<int:bid>/p/<int:pid>')
