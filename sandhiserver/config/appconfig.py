d47dc23383b34b29f8497c9424f10fc03e527624
venkatapathy <venkat.s.iyer@gmail.com>
2021-07-14 09:24:15 +0530

    a working model finally

---
+++ sandhiserver/config/appconfig.py
@@ -0,0 +1,4 @@
+class Config(object):
+    MONGODB_SETTINGS = {'host': 'mongodb://127.0.0.1:27017/sandhi-books'}
+    SANDHI_LIB_INPUT_DIR = '/home/server/Documents/ocr/sandhi/input_books'
+    SANDHI_LIB_OUTPUT_DIR = '/home/server/Documents/ocr/sandhi/output_books'
