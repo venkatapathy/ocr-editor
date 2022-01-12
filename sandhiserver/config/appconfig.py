09e78848555864f1a3945a3c3073baa9942e12a6
venkat.s.iyer@gmail.com <venkat.s.iyer@gmail.com>
2022-01-12 11:52:54 +0530

    sandhiserver to server

--- sandhiserver/config/appconfig.py
+++
@@ -1,4 +0,0 @@
-class Config(object):
-    MONGODB_SETTINGS = {'host': 'mongodb://127.0.0.1:27017/sandhi-books'}
-    SANDHI_LIB_INPUT_DIR = '/home/ruralivrs/sandhi/input_books'
-    SANDHI_LIB_OUTPUT_DIR = '/home/ruralivrs/sandhi/output_books'
--- sandhiserver/startapp.sh
+++
@@ -1,2 +0,0 @@
-export FLASK_ENV=development
-flask run --host=0.0.0.0
