1b2d7794226ac730a7e72dba9b10ee07b3b886eb
venkat.s.iyer@gmail.com <venkat.s.iyer@gmail.com>
2022-01-16 10:25:31 +0530

    making some directory changes

--- frontend/sandhi-web/src/reducerContext.ts
+++
@@ -1,7 +0,0 @@
-import { createReducerContext } from 'react-use';
-
-import { reducer, initialState } from './reducer';
-
-
-
-export const [useAppReducer, AppReducerProvider, AppReducerContext] = createReducerContext(reducer, initialState);
