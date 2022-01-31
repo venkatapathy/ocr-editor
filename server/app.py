1d22f8594fc465b3c5ef589f9e3a37fdf759f9d4
sandydasari <96286740+sandydasari@users.noreply.github.com>
2022-01-31 21:22:10 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,19 +1,26 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, render_template, request
 from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS  #comment this on deployment
 from mgodb.db import initialize_db
 from api.routes import initialize_routes
 from config.appconfig import Config
+from flask_sqlalchemy import SQLAlchemy
+import models
+from models import initialize_db
 
 app = Flask(__name__, static_url_path='', static_folder='static')
 CORS(app)  #comment this on deployment
 api = Api(app)
 
+app.config['SANDHI_LIB_INPUT_DIR'] = 'C:/Users/HP/sandhi/input/'
+app.config['SANDHI_LIB_OUTPUT_DIR'] = 'C:/Users/HP/sandhi/output/'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #app.config['MONGODB_SETTINGS'] = {
 #    'host': 'mongodb://127.0.0.1:27017/sandhi-books'
 #}
 
-app.config.from_object(Config())
+#app.config.from_object(Config())
 
 initialize_db(app)
 
